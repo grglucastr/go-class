125feb5002a1745646eb5b7680909f7fb57f9eb7
= <george.bentes@gmail.com>
2024-01-09 07:53:31 -0300

    add: strings vars

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// strings
+	var nameOne string = "George"
+	var nameTwo = "Lucas"
+	var nameThree string
+
+	fmt.Println(nameOne, nameTwo)
+	fmt.Println(nameTwo)
+	fmt.Println(nameThree)
+
+	nameOne = "peach"
+	nameThree = "bowser"
+
+	fmt.Println(nameOne, nameThree)
+
+	nameFour := "yoshi"
+
+	fmt.Println(nameFour)
 }
