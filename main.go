7a06d5baee6f350a690bf719e46bdfdab45d45f8
= <george.bentes@gmail.com>
2024-01-11 07:01:49 -0300

    add: examples of Standard Library usage for strings

--- main.go
+++ main.go
@@ -1,35 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var ages[3] int = [3] int {20, 25, 32}
-	var agess = [3]int {20, 25, 32}
+	greeting := "hello there friends!"
 
-	names := [4]string {"yoshi", "mario", "peach", "bowser"}
-	names[0] = "luigi"
+	fmt.Println(strings.Contains(greeting, "friends"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "th"))
+	fmt.Println(strings.Split(greeting, " "))
 
-	fmt.Println(ages, len(ages))
-	fmt.Println(agess, len(agess))
-	fmt.Println(names, len(names))
-
-	// SLICES
-
-	var scores = []int {100, 50, 10}
-	scores[2] = 25
-
-	scores = append(scores, 85)
-	fmt.Println(scores, len(scores))
-
-	// SLICE RANGES
-
-	rangeOne := names[1:3]
-	fmt.Println(rangeOne)
-
-	rangeTwo := names[2:]
-	fmt.Println(rangeTwo)
-
-	rangeThree := names[:3] //do not include the the third one
-	fmt.Println(rangeThree)
-
+	// the original one
+	fmt.Println("original string value=", greeting)
 }
