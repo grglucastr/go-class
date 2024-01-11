1cb9e2f2cec79225caca7101520b27600245b33e
= <george.bentes@gmail.com>
2024-01-11 07:40:31 -0300

    add: creating simple functions

--- main.go
+++ main.go
@@ -4,37 +4,17 @@ import (
 	"fmt"
 )
 
+func sayGreeting(name string){
+	fmt.Printf("Good morning %v \n", name)
+}
+
+func sayBye(name string){
+	fmt.Printf("Goodbye %v\n", name)
+}
+
 func main() {
+	sayGreeting("mario")
+	sayGreeting("luigi")
 
-	age := 45
-
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
-
-	if age < 30 {
-		fmt.Println("age is less than 30")
-	} else if age < 40 {
-		fmt.Println("age is less than 40")
-	} else {
-		fmt.Println("age is not less than 45")
-	}
-
-	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continuing at pos ", 1)
-			continue
-		}
-
-		if index > 2 {
-			fmt.Println("breaking at pos ", index)
-			break
-		}
-
-		fmt.Printf("the value at pos %v is %v\n", index, value)
-	}
-
+	sayBye("mario")
 }
