a3ac4aafbd39c00a9b5d998b23ca84fc40f5b553
= <george.bentes@gmail.com>
2024-01-11 07:20:48 -0300

    add: while loop

--- main.go
+++ main.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 60)
-	fmt.Println(index)
-
-	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
-	sort.Strings(names)
-	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "hugo"))
-
+	x := 0
+	for x < 5 {
+		fmt.Println("the value of x is ", x)
+		x++
+	}
+	
 }
