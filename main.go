a47979a7f9468d5f3c7a929a4d391f588a1b2fe6
= <george.bentes@gmail.com>
2024-01-11 07:18:30 -0300

    add: Sorting and Searching arrays with Standard Library

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	greeting := "hello there friends!"
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-	fmt.Println(strings.Contains(greeting, "friends"))
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "th"))
-	fmt.Println(strings.Split(greeting, " "))
+	sort.Ints(ages)
+	fmt.Println(ages)
 
-	// the original one
-	fmt.Println("original string value=", greeting)
+	index := sort.SearchInts(ages, 60)
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+	sort.Strings(names)
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "hugo"))
+
 }
