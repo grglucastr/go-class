7aa5d5392b1c61a0b7edb1f6139c17cb0e365d24
= <george.bentes@gmail.com>
2024-01-11 06:53:06 -0300

    add: slice ranges

--- main.go
+++ main.go
@@ -14,11 +14,22 @@ func main() {
 	fmt.Println(names, len(names))
 
 	// SLICES
-	
+
 	var scores = []int {100, 50, 10}
 	scores[2] = 25
 
 	scores = append(scores, 85)
 	fmt.Println(scores, len(scores))
+
+	// SLICE RANGES
+
+	rangeOne := names[1:3]
+	fmt.Println(rangeOne)
+
+	rangeTwo := names[2:]
+	fmt.Println(rangeTwo)
+
+	rangeThree := names[:3] //do not include the the third one
+	fmt.Println(rangeThree)
 
 }
