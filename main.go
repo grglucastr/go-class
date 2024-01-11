e0636af8ed5c9c569201f102034b0f1514de5e89
= <george.bentes@gmail.com>
2024-01-11 07:32:12 -0300

    add: basics of booleans and conditionals

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 
 func main() {
 
-	names := []string {"mario", "luigi", "yoshi", "peach"}
-	
-	for _, value := range names {
-		fmt.Printf("the value is %v \n", value)
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if  age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
 	}
 
 }
