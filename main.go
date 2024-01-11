bff18a8fec4e1270a569aa1f88909d58a4f5443a
= <george.bentes@gmail.com>
2024-01-11 06:49:57 -0300

    add: basic of slices

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ func main() {
 	var agess = [3]int {20, 25, 32}
 
 	names := [4]string {"yoshi", "mario", "peach", "bowser"}
+	names[0] = "luigi"
 
 	fmt.Println(ages, len(ages))
 	fmt.Println(agess, len(agess))
 	fmt.Println(names, len(names))
+
+	// SLICES
+	
+	var scores = []int {100, 50, 10}
+	scores[2] = 25
+
+	scores = append(scores, 85)
+	fmt.Println(scores, len(scores))
 
 }
