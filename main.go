69c45a1c7c627a31b27d921c167282092ef9cd1c
= <george.bentes@gmail.com>
2024-01-11 07:48:40 -0300

    add: function returning a value

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func sayGreeting(name string){
@@ -18,11 +19,22 @@ func cycleNames(names[] string, f func(string)){
 	}
 }
 
+func circleArea(radius float64) float64 {
+	return math.Pi * radius * radius
+}
+
 func main() {
 
 	names := []string{"cloud", "tifa", "barret"}
 	
 	cycleNames(names, sayGreeting)
 	cycleNames(names, sayBye)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+
+	fmt.Println(a1, a2)
+	fmt.Printf("circle 1 is %0.3f and circle 3 is %0.3f\n", a1, a2)
+
 
 }
