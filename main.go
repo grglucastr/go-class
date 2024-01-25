183c26ece243c09bc33da8b62dab0255d62e674a
= <george.bentes@gmail.com>
2024-01-25 14:51:26 -0300

    add: switch statements

--- main.go
+++ main.go
@@ -17,20 +17,42 @@ func createBill() bill {
 
 	reader := bufio.NewReader(os.Stdin)
 	name, _ := getInput("Create a new bill name: ", reader)
-	
+
 	b := newBill(name)
 	fmt.Println("Bill created.")
 
 	return b
 }
 
-func promptOptions (b *bill){
+func promptOptions(b *bill) {
 
 	reader := bufio.NewReader(os.Stdin)
 
 	fmt.Println()
 	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		fmt.Println(name, price)
+		break
+	case "s":
+		fmt.Println("Save Bill")
+		break
+	case "t":
+
+		tip, _ := getInput("Enter tip amount ($): ",reader)
+		fmt.Println(tip)
+		
+		break
+	default:
+		fmt.Println("Invalid option.")
+		promptOptions(b)
+		break
+	}
 
 }
 
